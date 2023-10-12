1d9f1bb22d52a54517bb478ef8a14f57cae8b0e0
LabA1_PC11 <cadizsalanovajarodzarian@gmail.com>
2023-10-12 08:29:57 -0400

    prueba

---
+++ practicas/src/ejercicio2_1/main.java
@@ -0,0 +1,10 @@
+package ejercicio2_1;
+
+public class main {
+    public static void main(String args[]) {
+    ejercicio p1 = new ejercicio("Pedro","Perez","1053121010","Bolivia",'H',1998);
+    ejercicio p2 = new ejercicio("Luis","León","1053223344","Bolivia",'H',2001);
+    p1.imprimir();
+    p2.imprimir();
+}
+}
